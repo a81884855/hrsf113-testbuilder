4c72ae653cde228af9516b86f9ff8e2d3dca8cbb
jiafeng guan <garyguan@jiafengs-MacBook-Pro.local>
2019-02-11 12:28:57 -0800

    Second Attempt

--- detectNetwork.js
+++ detectNetwork.js
@@ -8,6 +8,38 @@
 //   2. The number of digits in the number (called the length)
 
 var detectNetwork = function(cardNumber) {
+	var cardNumber = cardNumber.toString();
+	var len = cardNumber.length; 
+	var prefix2 = cardNumber.split("").slice(0,2).join("");
+	var prefix3 = cardNumber.split("").slice(0,3).join("");
+	var prefix4 = cardNumber.split("").slice(0,4).join("");
+	var prefix6 = cardNumber.split("").slice(0,6).join("");
+
+	var discover = [];
+	for (var i = 644; i <= 649; i++) {
+		discover.push(i.toString());
+	}
+
+	var unionpay = ['624','625','626'];
+	for (var i = 622126; i <= 622925; i++) {
+		unionpay.push(i.toString());
+	}
+
+	for (var i = 6282; i <= 6288; i++) {
+		unionpay.push(i.toString());
+	}
+
+	var Switch = ['4903', '4905', '4911', '4936', '564182', '633110', '6333', '6759'];
+
+
+	if (len === 14 && (prefix2 === "38"|| prefix2=="39")){return "Diner's Club"}
+	else if(len === 15 && (prefix2 === "34" || prefix2 === "37")){return "American Express"}
+	else if((len === 13 || len ==16 || len === 19) && prefix4 === '4123'){return "Visa"}
+	else if((prefix2 === '51' || prefix2 === "52" || prefix2 === "53" || prefix2 === "54" || prefix2 === "55") && len === 16){return "MasterCard"}
+	else if((prefix2 === '65' || prefix4 === '6011' || discover.includes(prefix3)) && (len === 16 || len ===19)){return "Discover"}
+	else if((prefix4 === "5018" || prefix4 === "5020" || prefix4 === "5038" || prefix4 === '6304') && (len >= 12 || len <= 19)){return "Maestro"}
+	else if((unionpay.includes(prefix4)|| unionpay.includes(prefix6) || unionpay.includes(prefix3)) && (len>=16 && len <=19)){return "China UnionPay"}
+	else if((Switch.includes(prefix6) || Switch.includes(prefix4)) && (len === 16 || len === 18 || len ===19)){return "Switch"}
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
